954ae9bdd433b410e13f61646ed54cc4444c12c3
Francesco Bianco <info.francescobianco@gmail.com>
2020-05-08 17:52:26 +0200

    release

--- Makefile
+++ Makefile
@@ -6,8 +6,9 @@ require:
 dump-autoload:
 	docker run --rm -v $${PWD}:/app -u $$(id -u) composer dump-autoload
 update-readme:
-	docker run --rm -v $${PWD}:/app -u $$(id -u) php -f /app/update-readme.php
-
+	docker run --rm -v $${PWD}:/app -u $$(id -u) php -f /app/scripts/update-readme.php
+test:
+	./scripts/test-runner.sh
 release: update-readme
 	git add .
 	git commit -am "release"
--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -2,6 +2,7 @@
 
 namespace Javanile\Hamper\Tests;
 
+use Javanile\Hamper\Hamper;
 use PHPUnit\Framework\TestCase;
 
 class DatabaseTest extends TestCase
@@ -9,6 +10,28 @@ class DatabaseTest extends TestCase
     public function testInfo()
     {
         $hdb = Hamper::getInstance();
+
+
+
+
+
+
+
+        $row = Hamper::fetch($sql);
+
+
+        var_dump($row);
+
+        $rows = Hamper::fetchAll($sql);
+        var_dump($rows);
+
+        $adb = Hamper::getDatabase();
+
+        $hdb->insert('vtiger_table_name', [
+            'field1' => 'value1',
+        ]);
+
+
 
         $hdb->fecth("SQL asdasd");
 

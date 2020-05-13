df9932b2749e7ed0fc9fe35a2a47f294b45a8b4f
RC2PC2 <rcsquaredpcsquared@gmail.com>
2020-05-13 14:39:49 +0200

    test: added first test implementation of both insert() and update() functions in HamperDatabaseTest.php. Test currently throws ERROR, DO NOT USE.

--- tests/HamperDatabaseTest.php
+++ tests/HamperDatabaseTest.php
@@ -32,8 +32,6 @@ class HamperDatabaseTest extends TestCase
             exit(1);
         }
 
-        var_dump($row);
-
         $this->assertEquals($key, $row['field2']);
     }
     public function testFetchAll()
@@ -60,9 +58,74 @@ class HamperDatabaseTest extends TestCase
 
         $this->assertEquals($key, $row[0]['field2']);
     }
-    // public function testInsert(){}
+    /*
+    public function testInsert()
+    {
+        $hdb = Hamper::getInstance();
+        $key1 = md5(time().rand());
+        $key2 = md5(time().rand());
 
-    // public function testUpdate(){}
+        $sql = "DROP TABLE IF EXISTS test";
+        $res = $hdb->query($sql, []);
+
+        $sql = "CREATE TABLE IF NOT EXISTS test (field_1 TEXT, field_2 TEXT) ENGINE=INNODB";
+        $res = $hdb->query($sql, []);
+
+        $data = [
+                'field_1' => $key1,
+                'field_2' => $key2,
+                ];
+
+        $keys = array_keys($data);
+        $values = array_values($data);
+        $row = $hdb->insert("test", $data, ['dieOnError' => true]);
+        var_dump($row);
+
+        $sql = "SELECT * FROM test WHERE field1 = ?";
+        $res = $hdb->query($sql, [$key1]);
+
+        $this->assertEquals($key1, $row[0]['field1']);
+
+    }
+    */
+    public function testUpdate()
+    {
+        $hdb = Hamper::getInstance();
+        $key1 = md5(time().rand());
+        $key2 = md5(time().rand());
+        $key3 = md5(time().rand());
+        $key4 = md5(time().rand());
+
+        $sql = "DROP TABLE IF EXISTS test";
+        $res = $hdb->query($sql, []);
+
+        $sql = "CREATE TABLE IF NOT EXISTS test (field_1 TEXT, field_2 TEXT) ENGINE=INNODB";
+        $res = $hdb->query($sql, []);
+
+        $sql = "INSERT INTO test (field1, field2) VALUES (?, ?)";
+        $res = $hdb->query($sql, [$key1, $key2]);
+
+        $sql = "INSERT INTO test (field1, field2) VALUES (?, ?)";
+        $res = $hdb->query($sql, [md5(time().rand()), md5(time().rand())]);
+
+
+        $recordToChange = [
+            'field_1' => $key1,
+            'field_2' => $key2,
+        ];
+
+
+        $data = [
+                'field_1' => $key3,
+                'field_2' => $key4,
+                ];
+
+        $row = $hdb->update("test", $recordToChange, $data, ['dieOnError' => true]);
+        var_dump($row);
+
+        $this->assertEquals($key3, $row[0]['field1']);
+
+    }
 
     // public function testDelete(){}
 }

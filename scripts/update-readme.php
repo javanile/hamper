f4c53176167526448567a8ac03bcff932b5c7812
francescobianco <info.francescobianco@gmail.com>
2020-10-08 23:58:35 +0200

    test

--- scripts/update-readme.php
+++ scripts/update-readme.php
@@ -7,7 +7,7 @@ use Javanile\Hamper\HamperDatabaseTables;
 use phpDocumentor\Reflection\DocBlockFactory;
 
 $toc = '';
-$documentation = '';
+$documentation = "<hr/>"."\n\n";
 $docBlockFactory = DocBlockFactory::createInstance();
 
 $sections = [
@@ -15,20 +15,23 @@ $sections = [
     HamperDatabaseTables::class => 'Tables manipulation',
 ];
 
-foreach ($sections as $class => $title) {
-    $toc .= '* ['.$title.'](#'.strtr($title, ' ', '-').')'."\n";
-    foreach (get_class_methods($class) as $method) {
-        if ($method == '__construct') {
-            continue;
-        }
-        try {
+try {
+    foreach ($sections as $class => $title) {
+        $reflection = new ReflectionClass($class);
+        $docBlock = $docBlockFactory->create($reflection->getDocComment());
+        $summary = trim($docBlock->getSummary(), '.');
+        $toc .= '**'.$title.':** '.$summary."\n";
+        foreach (get_class_methods($class) as $method) {
+            if ($method == '__construct') {
+                continue;
+            }
             echo "> {$class}::{$method}\n";
             $reflection = new ReflectionMethod($class, $method);
-            $docBlock = $docBlockFactory ->create($reflection->getDocComment());
+            $docBlock = $docBlockFactory->create($reflection->getDocComment());
             $summary = trim($docBlock->getSummary(), '.');
             $description = $docBlock->getDescription();
             $usage = $docBlock->getTagsByName('usage')[0];
-            $toc .= '  * ['.$summary.'](#'.strtr($summary, ' ', '-').')'."\n";
+            $toc .= ' * ['.$summary.'](#'.strtr($summary, ' ', '-').')'."\n";
             $documentation .= '### ' . $summary . "\n\n";
             $documentation .= $description . "\n\n";
             $documentation .= "#### Usage \n\n```\n" . $usage->getDescription() . "\n```\n\n";
@@ -38,11 +41,11 @@ foreach ($sections as $class => $title) {
             }
             $documentation .= '[[back to top]](#Documentation)'."\n\n";
             $documentation .= "<hr/>"."\n\n";
-        } catch (ReflectionException $exception) {
-            echo $exception->getMessage()."\n";
-            exit(1);
         }
     }
+} catch (ReflectionException $exception) {
+    echo $exception->getMessage()."\n";
+    exit(1);
 }
 
 $readme = file_get_contents('/app/README.md');
--- src/HamperDatabase.php
+++ src/HamperDatabase.php
@@ -5,14 +5,6 @@ namespace Javanile\Hamper;
 use Javanile\Hamper\PearDatabaseDecorator;
 
 /**
- * Class HamperDatabase is a PearDatabase wrapper used to fulfill the lack
- * of versatility and test-control on the PearDatabase vTiger object.
- *
- * Due to weaknesses as SQL Injection and other possible security weaknesses
- * within the context of Hamper and its query wrapping functions, it has been
- * chosen to always use parametric queries, and not to allow in any case to
- * use a standard SQL query.
- *
  * The long-term intentions of this own wrapper is to facilitate the usage
  * of PearDatabase and to make unit-testing available to any vTiger developer
  * since DBs are as important as any other logical element, and so it should
